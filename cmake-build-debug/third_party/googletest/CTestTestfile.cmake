94153dda4bfe636a39725ba5f792edbd627c0314
Tingan Ho <tingan87@gmail.com>
2020-06-25 22:51:16 +0200

    !chore: Added gitignore

--- cmake-build-debug/third_party/googletest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/tinganho/Workspace/Elet/third_party/googletest
-# Build directory: /Users/tinganho/Workspace/Elet/cmake-build-debug/third_party/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
