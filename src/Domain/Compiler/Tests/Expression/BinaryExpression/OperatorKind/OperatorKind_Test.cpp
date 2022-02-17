7375355df98ebfb2564291352bdc06dc5070d64c
Tingan Ho <tingan87@gmail.com>
2022-02-17 20:29:24 +0100

    test: Fixes operator stash ir tests

--- src/Domain/Compiler/Tests/Expression/BinaryExpression/OperatorKind/OperatorKind_Test.cpp
+++ src/Domain/Compiler/Tests/Expression/BinaryExpression/OperatorKind/OperatorKind_Test.cpp
@@ -35,15 +35,15 @@ namespace elet::domain::compiler::test
     }
 
 
-    TEST_F(OperatorKindFixture, Operator_Subtract)
+    TEST_F(OperatorKindFixture, Operator_Subtract_32)
     {
         testMainFunction(
-            "var x = 3;\n"
-            "var y = 3;\n"
-            "return x - y;");
+            "var x: s32 = 3;\n"
+            "var y: s32 = 3;\n"
+            "return x - y;", "s32");
 
         EXPECT_TRUE(testProject({
-            .baselineName = "Operator_Subtract",
+            .baselineName = "Operator_Subtract_32",
             .targets = {
                 CompilationTarget::StashIR,
             },
@@ -51,15 +51,15 @@ namespace elet::domain::compiler::test
     }
 
 
-    TEST_F(OperatorKindFixture, Operator_Multiply)
+    TEST_F(OperatorKindFixture, Operator_Subtract_64)
     {
         testMainFunction(
-            "var x = 3;\n"
-            "var y = 3;\n"
-            "return x * y;");
+            "var x: s64 = 3;\n"
+            "var y: s64 = 3;\n"
+            "return x - y;", "s64");
 
         EXPECT_TRUE(testProject({
-            .baselineName = "Operator_Multiply",
+            .baselineName = "Operator_Subtract_64",
             .targets = {
                 CompilationTarget::StashIR,
             },
@@ -67,15 +67,63 @@ namespace elet::domain::compiler::test
     }
 
 
-    TEST_F(OperatorKindFixture, Operator_Divide)
+    TEST_F(OperatorKindFixture, Operator_Multiply_32)
     {
         testMainFunction(
-            "var x = 3;\n"
-            "var y = 3;\n"
-            "return x / y;");
+            "var x: s32 = 3;\n"
+            "var y: s32 = 3;\n"
+            "return x * y;", "s32");
 
         EXPECT_TRUE(testProject({
-            .baselineName = "Operator_Divide",
+            .baselineName = "Operator_Multiply_32",
+            .targets = {
+                CompilationTarget::StashIR,
+            },
+        }));
+    }
+
+
+    TEST_F(OperatorKindFixture, Operator_Multiply_64)
+    {
+        testMainFunction(
+            "var x: s64 = 3;\n"
+            "var y: s64 = 3;\n"
+            "return x * y;", "s64");
+
+        EXPECT_TRUE(testProject({
+            .baselineName = "Operator_Multiply_64",
+            .targets = {
+                CompilationTarget::StashIR,
+            },
+        }));
+    }
+
+
+    TEST_F(OperatorKindFixture, Operator_Divide_S32)
+    {
+        testMainFunction(
+            "var x: s32 = 3;\n"
+            "var y: s32 = 3;\n"
+            "return x / y;", "s32");
+
+        EXPECT_TRUE(testProject({
+            .baselineName = "Operator_Divide_S32",
+            .targets = {
+                CompilationTarget::StashIR,
+            },
+        }));
+    }
+
+
+    TEST_F(OperatorKindFixture, Operator_Divide_S64)
+    {
+        testMainFunction(
+            "var x: s64 = 3;\n"
+            "var y: s64 = 3;\n"
+            "return x / y;", "s64");
+
+        EXPECT_TRUE(testProject({
+            .baselineName = "Operator_Divide_S64",
             .targets = {
                 CompilationTarget::StashIR,
             },
